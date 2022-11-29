38fae4564cfc7ec4ff65bd01c1ff1e3f92255990
mikuweb <mxxx.kwtmg@gmail.com>
2022-11-28 20:42:17 -0800

    Add toggle function

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useReducer } from "react";
+import "./style.css";
+import Todo from "./Todo";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const ACTIONS = {
+  ADD_TODO: "add_todo",
+  TOGGLE_TODO: "toggle_todo",
+};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodos(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return {...todo, complete: !todo.complete}
+        }
+      return todo
+      })
+  }
 }
 
-export default App
+function newTodos(name) {
+  return { id: Date.now(), name: name, complete: false };
+}
+
+function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState("");
+
+  function handleSubit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
+  }
+
+  console.log(todos);
+
+  return (
+    <>
+    <form onSubmit={handleSubit}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </form>
+    {todos.map(todo => {
+return     <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+    })}
+    </>
+  );
+}
+
+export default App;
+
+// ==========================
+//・payload: {name: name} ⇒ 2つ目のname≒このname ⇒ const [name, setName] = useState("");
+// ==========================
